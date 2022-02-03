6195307df6186f1b81315947ddcffbd768d754e9
zed.shawn <alizeeshan1999@gmail.com>
2022-02-03 12:45:39 +0530

    added drawer

--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        title: 'CovidHelp',
+        headerTitleAlign: 'center',
+      }}>
+      <Drawer.Screen name="Feed" component={HomeScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 const Stack = createStackNavigator();
 
@@ -16,5 +31,22 @@ function MainStack() {
     </Stack.Navigator>
   );
 }
+const Final = createStackNavigator();
 
-export default MainStack;
+const FinalStack = () => {
+  return (
+    <Final.Navigator>
+      <Final.Screen
+        name="Drawer"
+        component={DrawerNavigator}
+        options={{headerShown: false}}
+      />
+      <Final.Screen
+        name="MainStack"
+        component={MainStack}
+        //options={{headerShown: false}}
+      />
+    </Final.Navigator>
+  );
+};
+export default FinalStack;
